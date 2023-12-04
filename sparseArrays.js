136cd8bf1ad6796187ea85acb595b5baf052c41f
AliMalik76 <64746004+AliMalik76@users.noreply.github.com>
2023-12-04 08:29:26 +0500

    Create sparseArrays.js

---
+++ sparseArrays.js
@@ -0,0 +1,33 @@
+   //---------------O(n^2) solution-----------------
+    let occurences = []
+    for(let i=0; i<queries.length; i++){
+        occurences[i] = 0
+        for (let j = 0; j<stringList.length;j++){
+            if(queries[i] === stringList[j]){
+                occurences[i]++
+            }
+        }
+    }
+    return occurences
+    //-----------------------------------------------
+    
+    //---------------O(n) solution-----------------
+    let occurences = {}
+    let results = []
+    stringList.forEach((el)=>{
+        if(occurences[el]){
+            occurences[el]++
+        }else{
+            occurences[el] = 1
+        }
+    })
+    
+    queries.forEach((el)=>{
+        if(occurences[el]){
+            results.push(occurences[el])
+        }else{
+            results.push(0)
+        }
+    })
+    return results
+    //---------------------------------------------
